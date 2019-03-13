8adf3f0d4bfdf989d5cec62cb9d214081e62ee2d
Jessica Hong <jessicahong.24@gmail.com>
2019-03-13 15:27:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
  const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Star"];
  const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
-function theBeatlesPlay () {
-  return (musicians, instruments)
-}
-let aaray = [ ]
+function theBeatlesPlay (musicians, instruments) {
+
+let aaray = [ ] 
 for( let i=0; i < musicians.length; i++) {
   const str = musicians [i] + "plays" + instruments [i];
   array.push (str);
 } 
+return array
+}
