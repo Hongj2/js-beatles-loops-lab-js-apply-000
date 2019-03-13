34e07774d725f86fd80a47299f8ecffe0ea4507f
Jessica Hong <jessicahong.24@gmail.com>
2019-03-13 15:15:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// add solution here
+ const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Star"];
+ const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay () {
+  return (musicians, instruments)
+}
+let aaray = [ ]
+for(musicians ; play ; instruments){ 
+}
+
+function beatlesLoops (){
+   return (instruments musicians)
+}
