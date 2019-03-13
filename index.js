cbd4bc361f7999503f9db94ab0fff610a7a44966
Jessica Hong <jessicahong.24@gmail.com>
2019-03-13 15:25:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ function theBeatlesPlay () {
   return (musicians, instruments)
 }
 let aaray = [ ]
-for(
+for( let i=0; i < musicians.length; i++) {
+  const str = musicians [i] + "plays" + instruments [i]:
+  array.push (str);
+} 
